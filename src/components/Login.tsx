b6bbae89ec3eaf29c690d4b096c484956490edeb
Bae Tae Young <sk8bty@gmail.com>
2024-10-12 18:21:53 +0900

    Login.tsx 업데이트

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -19,65 +19,65 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-white">
-      <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg rounded-lg w-full max-w-md">
-        <img 
-          src="https://i.ibb.co/KmS3Vwr/DALL-E-2024-10-11-17-59-36-Main-image-for-a-math-skills-diagnostic-service-using-AI-with-an-X-ray-th.webp" 
-          alt="Math Skills Diagnostic Service" 
-          className="w-full h-auto mb-6 rounded-lg object-cover"
-        />
-        <h3 className="text-3xl font-bold text-center text-blue-600 mb-6">Math-Ray 로그인</h3>
-        <form onSubmit={handleLogin}>
-          <div className="mt-4">
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                사용자 이름
-              </label>
-              <div className="relative">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <User className="h-5 w-5 text-gray-400" />
-                </span>
-                <input
-                  type="text"
-                  placeholder="사용자 이름"
-                  id="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                />
-              </div>
-            </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                비밀번호
-              </label>
-              <div className="relative">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LogIn className="h-5 w-5 text-gray-400" />
-                </span>
-                <input
-                  type="password"
-                  placeholder="비밀번호"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                />
-              </div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <button
-                type="submit"
-                className="w-full px-6 py-3 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-150 ease-in-out"
-              >
-                로그인
-              </button>
+  <div className="flex items-center justify-center h-screen bg-gradient-to-b from-blue-100 to-white">
+    <div className="px-4 py-4 text-left bg-white shadow-lg rounded-lg w-full max-w-md">
+      <img 
+        src="https://i.ibb.co/KmS3Vwr/DALL-E-2024-10-11-17-59-36-Main-image-for-a-math-skills-diagnostic-service-using-AI-with-an-X-ray-th.webp" 
+        alt="Math Skills Diagnostic Service" 
+        className="w-full h-auto mb-4 rounded-lg object-cover"
+      />
+      <h3 className="text-2xl font-bold text-center text-blue-600 mb-4">Math-Ray 로그인</h3>
+      <form onSubmit={handleLogin}>
+        <div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-1" htmlFor="username">
+              사용자 이름
+            </label>
+            <div className="relative">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <User className="h-5 w-5 text-gray-400" />
+              </span>
+              <input
+                type="text"
+                placeholder="사용자 이름"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
             </div>
           </div>
-        </form>
-      </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-1" htmlFor="password">
+              비밀번호
+            </label>
+            <div className="relative">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <LogIn className="h-5 w-5 text-gray-400" />
+              </span>
+              <input
+                type="password"
+                placeholder="비밀번호"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              />
+            </div>
+          </div>
+          <div className="flex items-baseline justify-between">
+            <button
+              type="submit"
+              className="w-full px-6 py-3 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-150 ease-in-out"
+            >
+              로그인
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-  );
+  </div>
+);
 };
 
 export default Login;
