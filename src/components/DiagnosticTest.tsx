1b68da9b17dceddd28aaca802157af17c91a0eda
Bae Tae Young <sk8bty@gmail.com>
2024-10-12 18:09:22 +0900

    DiagnosticTest.tsx 업데이트

--- src/components/DiagnosticTest.tsx
+++ src/components/DiagnosticTest.tsx
@@ -127,7 +127,7 @@ const DiagnosticTest: React.FC = () => {
     <Header />
     <div className="container mx-auto p-4 bg-gradient-to-b from-blue-100 to-white min-h-screen mt-20">
       <div className="max-w-3xl mx-auto bg-white rounded-lg shadow-lg p-8">
-        <div className="flex flex-wrap justify-between items-center mb-6">
+        /*<div className="flex flex-wrap justify-between items-center mb-6">
           <h1 className="text-2xl sm:text-3xl font-bold text-blue-600 mr-4">Math-Ray 진단평가</h1>
           <button
             onClick={handleLogout}
@@ -136,7 +136,7 @@ const DiagnosticTest: React.FC = () => {
             <LogOut className="mr-1 sm:mr-2 h-4 w-4 sm:h-5 sm:w-5" />
             로그아웃
           </button>
-        </div>
+        </div>*/
         <div className="flex justify-between items-center mb-6">
           <p className="text-lg font-semibold">
             문제 {answeredQuestions.length + 1} / 10
