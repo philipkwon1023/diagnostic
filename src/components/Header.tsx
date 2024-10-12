c588227df3eba59891e54f8b8ba83276b921f9d7
Bae Tae Young <sk8bty@gmail.com>
2024-10-12 18:08:01 +0900

    Header.tsx 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,13 +14,13 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-blue-600 text-white p-4 shadow-md fixed top-0 left-0 w-full z-50">
+    <header className="bg-blue-600 text-white p-1 shadow-md fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold cursor-pointer" onClick={() => navigate('/')}>
           Math-Ray
         </h1>
         <nav className="flex items-center w-full">
-          <button
+          /*<button
             onClick={() => navigate('/test')}
             className="hover:underline mr-4"
           >
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
             className="hover:underline mr-4"
           >
             결과 보기
-          </button>
+          </button>*/
           <button
             onClick={handleLogout}
             className="ml-auto flex items-center bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-150 ease-in-out"
