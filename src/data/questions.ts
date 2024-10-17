19b66638e392b6c7ab81c75479c2726975d5aa75
Bae Tae Young <sk8bty@gmail.com>
2024-10-17 21:09:39 +0900

    Update questions.ts

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -24,7 +24,7 @@ export const questions: Question[] = [
     {
         "id": 3,
         "text": "$\sqrt[3]{a}=3, \sqrt[4]{b}=5$ 일 때, $\sqrt[6]{a b}=3^p \times 5^q$ 이다. 이를 만족시키는 유리수 $p, q$ 에 대하여 $p q$ 의 값을 구하시오. (단, $a>0, b>0$ )",
-        "options": [$$\frac{1}{3}$$, $$\frac{2}{5}$$, $$\frac{3}{4}$$, $$\frac{1}{4}$$, $$\frac{5}{6}$$],
+        "options": ["$$\frac{1}{3}$$", "$$\frac{2}{5}$$", "$$\frac{3}{4}$$", "$$\frac{1}{4}$$", "$$\frac{5}{6}$$"],
         "correctAnswer": 1,
         "difficulty": 1,
         "concept": 2,
@@ -44,7 +44,7 @@ export const questions: Question[] = [
     {
         "id": 5,
         "text": "소리의 진동수는 반음 1 개만큼 높아질 때마다 일정한 비율로 증가한다. 다음을 이용하여\n‘도'음보다 반음 4 개만큼 높은 ‘미음의 진동수는 '도’음의 진동수의 몇 배인지 구하시오.\n- 한 옥타브는 12 개의 반음으로 이루어져 있다.\n- 음이 한 옥타브 올라가면 진동수는 2 배가 된다.",
-        "options": [$$\sqrt[]{2}$$",
+        "options": ["$$\sqrt[]{2}$$",
             "$$\sqrt[4]{2}$$",$$\sqrt[]{3}$$",
             "$$\sqrt[2]{2}$$",
             "$$\sqrt[5]{2}$$"
@@ -67,7 +67,7 @@ export const questions: Question[] = [
     },
     {
         "id": 7,
-        "text": "오른쪽 그림과 같은 정육면체의 부피가 $\sqrt{3}$ 일 때, 삼각형 AFC 의 넓 이는 $\frac{\sqrt{3^m}}{2}$ 이다. 이때 서로소인 두 자연수 $m, n$ 에 대하여 $m+n$ 의 값 을 구하시오. (단, $n \geq 2$ )",
+        "text": "오른쪽 그림과 같은 정육면체의 부피가 $\sqrt{3}$ 일 때, 삼각형 AFC의 넓이는 $\frac{\sqrt{3^m}}{2}$ 이다. 이때 서로소인 두 자연수 $m, n$ 에 대하여 $m+n$ 의 값 을 구하시오. (단, $n \geq 2$ )",
         "options": ["$$ 11 $$", "$$ 13 $$", "$$ 3 $$", "$$ 8 $$", "$$ 7 $$"],
         "correctAnswer": 1,
         "difficulty": 2,
@@ -78,7 +78,7 @@ export const questions: Question[] = [
     {
         "id": 8,
         "text": "$\frac{1}{2^{-4}+1}+\frac{1}{2^{-2}+1}+\frac{1}{2^2+1}+\frac{1}{2^4+1}$ 을 간단히 하시오.",
-        "options": ["$$ 2 $$", "$$ 3 $$", "$$ 5 $$", "$$ 7 $$", $$\frac{1}{2}$$],
+        "options": ["$$ 2 $$", "$$ 3 $$", "$$ 5 $$", "$$ 7 $$", "$$\frac{1}{2}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 3,
@@ -88,7 +88,7 @@ export const questions: Question[] = [
     {
         "id": 9,
         "text": "다음 조건을 모두 만족시키는 1 이 아닌 세 양수 $a, b, c$ 에 대하여 $a c=b^k$ 이다. 이때 실수 $k$ 의 값을 구하시오.\n(가) $a^2$ 은 $b$ 의 세제곱근이다.\n(나) $b^3$ 은 $c$ 의 제곱근이다.",
-        "options": [$$\frac{37}{6}$$, $$\frac{31}{6}$$, $$\frac{25}{6}$$, "$$ 6 $$", "$$ 5 $$"],
+        "options": ["$$\frac{37}{6}$$", "$$\frac{31}{6}$$", "$$\frac{25}{6}$$", "$$ 6 $$", "$$ 5 $$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -128,7 +128,7 @@ export const questions: Question[] = [
     {
         "id": 13,
         "text": "$a>0$ 이고 $\sqrt{a}-\frac{1}{\sqrt{a}}=2$ 일 때, $\frac{a^3+a^2}{a+1}-\frac{a^{-3}+a^{-2}}{a^{-1}+1}$ 의 값은?",
-        "options": [$$24\sqrt[]{2}$$",
+        "options": ["$$24\sqrt[]{2}$$",
             "$$18\sqrt[2]{2}$$",
             "$$12\sqrt[2]{2}$$",
             "$$15\sqrt[2]{2}$$",
@@ -143,7 +143,7 @@ export const questions: Question[] = [
     {
         "id": 14,
         "text": "$2^x=3^{-y}, 9^y=6^z$ 일 때, $\frac{1}{x}-\frac{1}{y}$ 을 $z$ 를 이용하여 나타내면? (단, $x y z \neq 0$ )",
-        "options": [$$-\frac{2}{z}$$, $$-\frac{1}{z}$$, $$\frac{1}{z}$$, "$$ z $$", "$$ 2z $$"],
+        "options": ["$$-\frac{2}{z}$$", "$$-\frac{1}{z}$$", "$$\frac{1}{z}$$", "$$ z $$", "$$ 2z $$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 3,
@@ -163,7 +163,7 @@ export const questions: Question[] = [
     {
         "id": 16,
         "text": "오른쪽 그림과 같이 선분 AB 를 지름으로 하는 원 위의 두 점 $\mathrm{P}, \mathrm{Q}$ 에 대하여 두 선분 $\mathrm{AB}, \mathrm{PQ}$ 의 교점을 R 라 하자. $\overline{\mathrm{AP}}=\overline{\mathrm{BP}}$ 이 고, $\overline{\mathrm{AR}}=4 \sqrt[3]{3}, \overline{\mathrm{BR}}=3 \sqrt[3]{3}$ 일 때, $\frac{5}{7} \overline{\mathrm{BQ}}$ 의 값은?",
-        "options": [$$\sqrt[]{81}$$",
+        "options": ["$$\sqrt[]{81}$$",
             "$$24\sqrt[3]{27}$$",$$24\sqrt[]{27}$$",
             "$$24\sqrt[3]{3}$$",$$24\sqrt[]{3}$$"
         ],
@@ -246,7 +246,7 @@ export const questions: Question[] = [
     {
         "id": 24,
         "text": "양수 $x, y, z$ 가 $\log _{12} x+\log _{12} 2 y+\log _{12} 3 z=1$ 을 만족시킬 때, $x y z$ 의 값을 구하시오.",
-        "options": ["$$ 2 $$", "$$ 3 $$", "$$ 1 $$", $$\frac{1}{2}$$, $$\frac{1}{3}$$],
+        "options": ["$$ 2 $$", "$$ 3 $$", "$$ 1 $$", "$$\frac{1}{2}$$", "$$\frac{1}{3}$$"],
         "correctAnswer": 1,
         "difficulty": 1,
         "concept": 2,
@@ -266,7 +266,7 @@ export const questions: Question[] = [
     {
         "id": 26,
         "text": "오른쪽 그림과 같이 $\overline{\mathrm{AB}}=3, \overline{\mathrm{AC}}=2$ 인 삼각형 ABC 에서 $\angle \mathrm{A}$ 의 이 등분선이 변 BC 와 만나는 점을 D 라 할 때, $\overline{\mathrm{BD}}=\log _3 x^2, \overline{\mathrm{CD}}=\log _3 y$ 이 다. 1 보다 큰 두 양수 $x, y$ 에 대하여 $x=y^k$ 일 때, 상수 $k$ 의 값을 구하시 오.",
-        "options": [$$\frac{3}{4}$$, $$\frac{1}{2}$$, $$\frac{3}{5}$$, $$\frac{2}{3}$$, $$\frac{5}{4}$$],
+        "options": ["$$\frac{3}{4}$$", "$$\frac{1}{2}$$", "$$\frac{3}{5}$$", "$$\frac{2}{3}$$", "$$\frac{5}{4}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -286,7 +286,7 @@ export const questions: Question[] = [
     {
         "id": 28,
         "text": "$\log 40=n+\alpha \quad(n$ 은 정수, $0 \leq \alpha<1)$ 일 때, $\frac{10^n+10^\alpha}{10^n-10^\alpha}$ 의 값은?",
-        "options": [$$\frac{7}{3}$$, $$\frac{7}{2}$$, $$\frac{11}{6}$$, "$$ 2 $$", "$$ 1 $$"],
+        "options": ["$$\frac{7}{3}$$", "$$\frac{7}{2}$$", "$$\frac{11}{6}$$", "$$ 2 $$", "$$ 1 $$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -296,7 +296,7 @@ export const questions: Question[] = [
     {
         "id": 29,
         "text": "$5^a=3,5^b=8$ 일 때, $\log _6 18$ 을 $a, b$ 에 대한 식으로 나타내면?",
-        "options": [$$\frac{6a+b}{3a+b}$$, $$\frac{6a-b}{3a+b}$$, $$\frac{6a+b}{3a-b}$$, $$\frac{6a-b}{3a-b}$$, $$\frac{3a+b}{6a+b}$$],
+        "options": ["$$\frac{6a+b}{3a+b}$$", "$$\frac{6a-b}{3a+b}$$", "$$\frac{6a+b}{3a-b}$$", "$$\frac{6a-b}{3a-b}$$", "$$\frac{3a+b}{6a+b}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 1,
@@ -306,7 +306,7 @@ export const questions: Question[] = [
     {
         "id": 30,
         "text": "이차방정식 $2 x^2-12 x+9=0$ 의 두 근을 $\alpha, \beta$ 라 할 때, $\log _{a^2}+\beta^2 2 \alpha+\log _{a^2+\hat{\beta}^2} \beta$ 의 값을 구하시오.",
-        "options": [$$\frac{2}{3}$$, "$$ 2 $$", $$\frac{1}{3}$$, "$$ 1 $$", $$\frac{1}{2}$$],
+        "options": ["$$\frac{2}{3}$$", "$$ 2 $$", "$$\frac{1}{3}$$", "$$ 1 $$", "$$\frac{1}{2}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -316,7 +316,7 @@ export const questions: Question[] = [
     {
         "id": 31,
         "text": "$\log _2 3=a, \log _3 5=b, \log _5 7=c$ 일 때, $\log _{14} 105$ 를 $a, b, c$ 에 대한 식으로 나타내시오.",
-        "options": [$$\frac{a + ab + ac}{1 + abc}$$, $$\frac{a + ab - ac}{1 - abc}$$,  \"$$\frac{a + ab + ac}{abc + 1}$$, $$\frac{a + ab + ac}{a + b + c}$$, $$\frac{a(b + c)}{1 + abc}$$],
+        "options": ["$$\frac{a + ab + ac}{1 + abc}$$", "$$\frac{a + ab - ac}{1 - abc}$$",  \"$$\frac{a + ab + ac}{abc + 1}$$", "$$\frac{a + ab + ac}{a + b + c}$$", "$$\frac{a(b + c)}{1 + abc}$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 2,
@@ -326,7 +326,7 @@ export const questions: Question[] = [
     {
         "id": 32,
         "text": "$\log _{x-3}\left(-x^2+8 x-12\right)$ 의 값이 존재하도록 하는 자연수 $x$ 에 대하여 $\log _2 x$ 의 정수 부분 을 $a$, 소수 부분을 $b$ 라 할 때, $2^{a-b}$ 의 값을 구하시오.",
-        "options": [$$\frac{16}{5}$$, $$\frac{4}{5}$$, $$\frac{8}{3}$$, "$$ 2 $$", $$\frac{12}{5}$$],
+        "options": ["$$\frac{16}{5}$$", "$$\frac{4}{5}$$", "$$\frac{8}{3}$$", "$$ 2 $$", "$$\frac{12}{5}$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 2,
@@ -416,7 +416,7 @@ export const questions: Question[] = [
     {
         "id": 41,
         "text": "함수 $f(x)=2^x$ 에 대하여 $f(p)=q$ 라 할 때, $f\left(\frac{p}{2}\right)+f\left(-\frac{p}{2}\right)$ 의 값은 $q$ 로 나타내면?",
-        "options": [$$\sqrt{q} + \frac{1}{\sqrt{q}}$$, $$\sqrt{q} - \frac{1}{\sqrt{q}}$$, $$\frac{1}{\sqrt{q}} - \sqrt{q}$$, $$\frac{1}{q} + \sqrt{q}$$, $$q + \frac{1}{q}$$],
+        "options": ["$$\sqrt{q} + \frac{1}{\sqrt{q}}$$", "$$\sqrt{q} - \frac{1}{\sqrt{q}}$$", "$$\frac{1}{\sqrt{q}} - \sqrt{q}$$", "$$\frac{1}{q} + \sqrt{q}$$", "$$q + \frac{1}{q}$$"],
         "correctAnswer": 1,
         "difficulty": 1,
         "concept": 1,
@@ -472,7 +472,7 @@ export const questions: Question[] = [
     {
         "id": 46,
         "text": "함수 $y=\left(a^2+a+1\right)^x$ 에서 $x$ 의 값이 증가할 때 $y$ 의 값은 감소하도록 하는 실수 $a$ 의 값의 범위를 구하시오.",
-        "options": [$$-1 < a < 0$$, $$0 < a < 1$$, $$a > 1$$, $$a < -1$$, $$a = 0$$],
+        "options": ["$$-1 < a < 0$$", "$$0 < a < 1$$", "$$a > 1$$", "$$a < -1$$", "$$a = 0$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -482,7 +482,7 @@ export const questions: Question[] = [
     {
         "id": 47,
         "text": "300. $0<a<1$ 일 때, 세 수 $a, a^a, a^{a^a}$ 의 대소 관계는?",
-        "options": [$$a < a^{a^{a}} < a^{a}$$, $$a^{a} < a^{a^{a}} < a$$, $$a^{a^{a}} < a < a^{a}$$, $$a^{a} < a < a^{a^{a}}$$, $$a < a^{a} < a^{a^{a}}$$],
+        "options": ["$$a < a^{a^{a}} < a^{a}$$", "$$a^{a} < a^{a^{a}} < a$$", "$$a^{a^{a}} < a < a^{a}$$", "$$a^{a} < a < a^{a^{a}}$$", "$$a < a^{a} < a^{a^{a}}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 2,
@@ -502,7 +502,7 @@ export const questions: Question[] = [
     {
         "id": 49,
         "text": "정의역이 $\{x \mid 0 \leq x \leq 3\}$ 인 함수 $f(x)=a^{x+1}$ 의 최댓값이 최솟값의 8 배가 되도록 하는 모든 양수 $a$ 의 값의 합을 구하시오.",
-        "options": [$$\frac{5}{2}$$, "$$ 2 $$", $$\frac{9}{4}$$, "$$ 1 $$", $$\frac{3}{2}$$],
+        "options": ["$$\frac{5}{2}$$", "$$ 2 $$", "$$\frac{9}{4}$$", "$$ 1 $$", "$$\frac{3}{2}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 4,
@@ -512,7 +512,7 @@ export const questions: Question[] = [
     {
         "id": 50,
         "text": "함수 $y=2^{-x+1}+k$ 의 그래프가 제 1 사분면을 지나지 않도록 하는 상수 $k$ 의 최댓값은?",
-        "options": ["$$ -2 $$", "$$ -1 $$", $$-\frac{1}{2}$$, "$$ 1 $$", "$$ 2 $$"],
+        "options": ["$$ -2 $$", "$$ -1 $$", "$$-\frac{1}{2}$$", "$$ 1 $$", "$$ 2 $$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 3,
@@ -522,7 +522,7 @@ export const questions: Question[] = [
     {
         "id": 51,
         "text": "함수 $y=\left(\frac{1}{2}\right)^{|x-1|}-5$ 의 그래프와 직선 $y=k$ 가 만나지 않도록 하는 실수 $k$ 의 값의 범위를 구하시오.",
-        "options": [$$k \leq -5 \text{ 또는 } k > -4$$, $$k \geq -5 \text{ 또는 } k < -4$$, $$k < -5 \text{ 또는 } k \geq -4$$, $$k > -5 \text{ 또는 } k < -4$$, $$k \leq -5 \text{ 또는 } k \geq -4$$],
+        "options": ["$$k \leq -5 \text{ 또는 } k > -4$$", "$$k \geq -5 \text{ 또는 } k < -4$$", "$$k < -5 \text{ 또는 } k \geq -4$$", "$$k > -5 \text{ 또는 } k < -4$$", "$$k \leq -5 \text{ 또는 } k \geq -4$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 3,
@@ -532,7 +532,7 @@ export const questions: Question[] = [
     {
         "id": 52,
         "text": "오른쪽 그림과 같이 $y$ 축 위의 두 점 $\mathrm{A}, \mathrm{B}$ 에 대하여 두 함수 $y=2^x, y=a^x$ 의 그래프와 점 B 를 지나는 직선 $y=k(k>1)$ 가 만나는 점을 각각 $\mathrm{C}, \mathrm{D}$ 라 하자. 삼각형 ACB 의 넓이가 삼각형 ADC 의 넓이의 2 배일 때, 상수 $a$ 의 값을 구하시오. (단, $1<a<2$ )",
-        "options": [$$\sqrt[]{4}$$",
+        "options": ["$$\sqrt[]{4}$$",
             "$$\sqrt[2]{4}$$",$$\sqrt[]{2}$$",$$\sqrt[]{8}$$",
             "$$\sqrt[2]{8}$$"
         ],
@@ -565,7 +565,7 @@ export const questions: Question[] = [
     {
         "id": 55,
         "text": "부등식 $\left(x^2-2 x+1\right)^{x-1}<1$ 의 해의 집합을 $S$ 라 할 때, 다음 중 집합 $S$ 의 원소가 아닌 것은? (단, $x \neq 1$ )",
-        "options": [$$\frac{1}{2}$$, $$\frac{3}{2}$$, $$-\frac{3}{2}$$, $$-\frac{1}{2}$$, $$-\frac{5}{2}$$],
+        "options": ["$$\frac{1}{2}$$", "$$\frac{3}{2}$$", "$$-\frac{3}{2}$$", "$$-\frac{1}{2}$$", "$$-\frac{5}{2}$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 6,
@@ -575,7 +575,7 @@ export const questions: Question[] = [
     {
         "id": 56,
         "text": "오른쪽 그림과 같이 직선 $x=a$ $(a>0)$ 가 두 함수 $y=8^x, y=4^x$ 의 그래프 및 $x$ 축과 만나는 점을 각각 $\mathrm{A}, \mathrm{B}, \mathrm{C}$ 라 하고, 점 A 에서 $y$ 축에 내린 수선의 발을 D , 점 B 를 지나고 $x$ 축에 평행한 직선이 함수 $y=8^x$ 의 그래프와 만나는 점을 E 라 하자. 이때 세 점 $\mathrm{D}, \mathrm{E}, \mathrm{C}$ 가 한 직선 위에 놓이도록 하는 $a$ 의 값을 구하시오.",
-        "options": [$$\log_{2}{3}$$, $$\log_{3}{2}$$, $$\log_{2}{6}$$, $$\log_{3}{6}$$, $$\log_{2}{5}$$],
+        "options": ["$$\log_{2}{3}$$", "$$\log_{3}{2}$$", "$$\log_{2}{6}$$", "$$\log_{3}{6}$$", "$$\log_{2}{5}$$"],
         "correctAnswer": 1,
         "difficulty": 4,
         "concept": 2,
@@ -595,7 +595,7 @@ export const questions: Question[] = [
     {
         "id": 58,
         "text": "$-2 \leq x \leq 2$ 에서 부등식 $a \cdot 2^{-x} \leq 2^{-2 x+1} \leq b \cdot 8^{-x}$ 이 성립할 때, 상수 $a, b$ 에 대하여 $b-a$ 의 최솟값은?",
-        "options": [$$\frac{15}{2}$$, "$$ 7 $$", "$$ 8 $$", $$\frac{17}{2}$$, "$$ 9 $$"],
+        "options": ["$$\frac{15}{2}$$", "$$ 7 $$", "$$ 8 $$", "$$\frac{17}{2}$$", "$$ 9 $$"],
         "correctAnswer": 1,
         "difficulty": 4,
         "concept": 5,
@@ -645,7 +645,7 @@ export const questions: Question[] = [
     {
         "id": 63,
         "text": "세 수 $A=2 \log _5 3, B=3, C=\log _{25} 115$ 의 대소 관계는?",
-        "options": [$$A < C < B$$, $$A < B < C$$, $$C < A < B$$, $$B < C < A$$, $$B < A < C$$],
+        "options": ["$$A < C < B$$", "$$A < B < C$$", "$$C < A < B$$", "$$B < C < A$$", "$$B < A < C$$"],
         "correctAnswer": 1,
         "difficulty": 1,
         "concept": 2,
@@ -695,7 +695,7 @@ export const questions: Question[] = [
     {
         "id": 68,
         "text": "이차방정식\n\n$$x^2-2\left(1+\log _3 a\right) x+4\left(1+\log _3 a\right)=0$$\n\n\n이 실근을 갖지 않도륵 하는 양수 $a$ 의 값의 범위는?",
-        "options": [$$\frac{1}{3} < a < 27$$, $$\frac{1}{4} < a < 27$$, $$a < \frac{1}{3}$$, $$a > 27$$, $$1 < a < 3$$],
+        "options": ["$$\frac{1}{3} < a < 27$$", "$$\frac{1}{4} < a < 27$$", "$$a < \frac{1}{3}$$", "$$a > 27$$", "$$1 < a < 3$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 5,
@@ -715,7 +715,7 @@ export const questions: Question[] = [
     {
         "id": 70,
         "text": "유입되는 불순믈의 $\frac{1}{5}$ 을 걸러내는 여과기가 있다. 이 여과기를 여러 개 겹쳐서 설치하여 전체 불순믈의 $\frac{1}{10}$ 만 여과기를 통과하게 하려고 한다. 이때 필요한 여과기의 개수를 구하시오.\n(단, $\log 2=0.3$ 으로 계산한다.)",
-        "options": [$$k > \frac{1}{4}$$, $$k < \frac{1}{4}$$, $$k = \frac{1}{4}$$, $$k \geq \frac{1}{4}$$, $$k \leq \frac{1}{4}$$],
+        "options": ["$$k > \frac{1}{4}$$", "$$k < \frac{1}{4}$$", "$$k = \frac{1}{4}$$", "$$k \geq \frac{1}{4}$$", "$$k \leq \frac{1}{4}$$"],
         "correctAnswer": 1,
         "difficulty": 2,
         "concept": 5,
@@ -725,7 +725,7 @@ export const questions: Question[] = [
     {
         "id": 71,
         "text": "부등식 $\left|\log _3 x\right|^2-\left|\log _3 x\right|-6<0$ 의 해를 구하시오.\n(단, $[x]$ 는 $x$ 보다 크지 않은 최대의 정수이다.)",
-        "options": [$$\frac{1}{3} \leq x < 27$$, $$x < \frac{1}{3}$$, $$x \geq 27$$, $$x \leq 27$$, $$\frac{1}{4} \leq x < 27$$],
+        "options": ["$$\frac{1}{3} \leq x < 27$$", "$$x < \frac{1}{3}$$", "$$x \geq 27$$", "$$x \leq 27$$", "$$\frac{1}{4} \leq x < 27$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 6,
@@ -755,7 +755,7 @@ export const questions: Question[] = [
     {
         "id": 74,
         "text": "$1<x<4$ 일 때, 세 수\n\n$$A=\log _2 x^2, B=\left(\log _2 x\right)^2, \quad C=\log _2\left(\log _2 x\right)$$\n\n\n의 대소 관계는?",
-        "options": [$$A > B > C$$, $$A > C > B$$, $$B > A > C$$, $$C > B > A$$, $$B > C > A$$],
+        "options": ["$$A > B > C$$", "$$A > C > B$$", "$$B > A > C$$", "$$C > B > A$$", "$$B > C > A$$"],
         "correctAnswer": 1,
         "difficulty": 3,
         "concept": 2,
@@ -785,7 +785,7 @@ export const questions: Question[] = [
     {
         "id": 77,
         "text": "직선 $y=x$ 위의\n\n한 점 P를 지나고 $x$ 축에 평행한 직선이 곡선\n$y=\log _4\left(x-\frac{1}{4}\right)$ 과\n만나는 점을 Q , 점 P 를 지나고 $y$ 축에 평행한\n직선이 곡선 $y=2^x$ 과 만나는 점을 R라 하자. 삼각형 PQR 가 $\overline{\mathrm{PQ}}=\overline{\mathrm{PR}}$ 인 이등변삼각형일 때, 삼각형 PQR 의 넓이는?",
-        "options": [$$\frac{9}{8}$$, $$\frac{7}{6}$$, $$\frac{8}{7}$$, $$\frac{10}{9}$$, $$\frac{11}{10}$$],
+        "options": ["$$\frac{9}{8}$$", "$$\frac{7}{6}$$", "$$\frac{8}{7}$$", "$$\frac{10}{9}$$", "$$\frac{11}{10}$$"],
         "correctAnswer": 1,
         "difficulty": 4,
         "concept": 2,
@@ -815,7 +815,7 @@ export const questions: Question[] = [
     {
         "id": 80,
         "text": "부등식 $\log _2(3 x+3) \geq \log _2\left(x^2+k\right)$ 들 만족시키는 정수 $x$ 의 개수가 2 일 때, 상수 $k$ 의 값의 범위를 구하시오. (단, $k>0$)",
-        "options": [$$3 \leq x < 5$$, $$2 \leq x < 5$$, $$2 < k < 3$$, $$k > 5$$, $$k < 3$$],
+        "options": ["$$3 \leq x < 5$$", "$$2 \leq x < 5$$", "$$2 < k < 3$$", "$$k > 5$$", "$$k < 3$$"],
         "correctAnswer": 1,
         "difficulty": 4,
         "concept": 6,
